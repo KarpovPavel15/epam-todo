7be980ea0d4e773de8a20f73309a0df2d2d6a8e5
KarpovPavel15 <kpvbygm@gmail.com>
2019-10-30 04:07:06 +0300

    add structure

--- src/Components/Content/Content.jsx
+++ src/Components/Content/Content.jsx
@@ -6,7 +6,31 @@ export default class Content extends Component {
         return(
             <main className="content-area">
                 <div className="content-area_main">
-                    jgghg
+                    <div className="content-area_main-twitArea">
+                        <div className="content-area_main-twitArea-textArea">
+                            <p className="content-area_main-twitArea-textArea-txt">All tips</p>
+                        </div>
+                        <div className="content-area_main-twitArea-btnArea">
+                            <input className="content-area_main-twitArea-btnArea-titleInput" hidden/>
+                            <input className="content-area_main-twitArea-btnArea-descriptionInput" hidden/>
+                            <button className="content-area_main-twitArea-btnArea-btn">
+                                Add tips
+                            </button>
+                        </div>
+                    </div>
+                    <div className="content-area_main-twitShowArea">
+                        <div className="content-area_main-twitShowArea-second">
+                            <div className="content-area_main-twitShowArea-second_block1">
+1
+                            </div>
+                            <div className="content-area_main-twitShowArea-second_block2">
+2
+                            </div>
+                            <div className="content-area_main-twitShowArea-second_block3">
+3
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </main>
         );
